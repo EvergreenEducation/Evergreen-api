7b93cadc2355695b758cca2b9e3e5776f75292d9
do-kevin <do.kevin.vo@gmail.com>
2020-04-03 22:31:07 -0700

    Attaches Offer controller
    Adds checks for updating provider

--- server/controllers/provider.js
+++ server/controllers/provider.js
@@ -1,4 +1,7 @@
-import { compact } from 'lodash';
+import {
+  compact, filter, map, property,
+  isEqual, differenceWith, find,
+} from 'lodash';
 
 import { Provider, DataFields } from '@/models';
 
@@ -33,12 +36,47 @@ export default class Controller {
     });
 
     this.providerResource.update.write_after(async (req, res, context) => {
-      let { type, topics } = req.body;
-      type = Number(type);
+      let providerInstance = await Provider.scope('with_datafields').findByPk(context.instance.id);
+      let {
+        type: newProviderType,
+        topics: newTopics,
+      } = req.body;
+
+      newProviderType = Number(newProviderType);
+
+      const myProviderType = find(providerInstance.dataValues.DataFields, ['type', 'provider']).dataValues.id;
+
+      const myTopics = map(
+        filter(providerInstance.dataValues.DataFields, ['type', 'topic']),
+        property('dataValues.id'),
+      );
+
+      const difference = differenceWith(myTopics, newTopics);
+
+      if (isEqual(myProviderType, newProviderType) && !difference.length) {
+        context.instance = providerInstance;
+        return context.continue;
+      }
+
+      if (!isEqual(myProviderType, newProviderType) && !difference.length) {
+        const newDataField = await DataFields.findOne({ where: { id: newProviderType } });
+
+        const withoutMyProviderType = filter(
+          providerInstance.dataValues.DataFields,
+          d => (d.dataValues.id !== myProviderType),
+        );
+
+        withoutMyProviderType.push(newDataField);
+
+        await context.instance.setDataFields(withoutMyProviderType);
+        providerInstance = await Provider.scope('with_datafields').findByPk(context.instance.id);
+        context.instance = providerInstance;
+        return context.continue;
+      }
 
       const datafields = compact([
-        type,
-        ...topics,
+        newProviderType,
+        ...newTopics,
       ]);
 
       if (datafields.length) {
@@ -49,12 +87,11 @@ export default class Controller {
         });
 
         await context.instance.setDataFields(datafield);
-        const providerInstance = await Provider.scope('with_datafields').findByPk(context.instance.id);
+        providerInstance = await Provider.scope('with_datafields').findByPk(context.instance.id);
         context.instance = providerInstance;
       }
 
       return context.continue;
     });
-
   }
 }
--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,7 @@ import finale from 'finale-rest';
 import UserController from './controllers/user';
 import ProviderController from './controllers/provider';
 import DataFieldsController from './controllers/datafields';
+import OfferController from './controllers/offer';
 import * as db from './models';
 
 export default function routes(app) {
@@ -15,4 +16,5 @@ export default function routes(app) {
   new UserController({ prefix: `${APP_PATH}/users`, app, finale });
   new ProviderController({ prefix: `${APP_PATH}/providers`, app, finale });
   new DataFieldsController({ prefix: `${APP_PATH}/datafields`, app, finale });
+  new OfferController({ prefix: `${APP_PATH}/offers`, app, finale });
 }
