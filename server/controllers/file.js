959a5e59e7690f6beac65145ae3857e5a776c15f
Lan Nguyen <lan-nguyen91>
2020-04-10 12:15:07 -0700

    add m2m helper

--- server/controllers/file.js
+++ server/controllers/file.js
@@ -1,4 +1,7 @@
-import { File } from '@/models';
+import {
+  File, Provider, Offer, OffersOffers,
+} from '@/models';
+import SequelizeHelperService from '@/services/sequelize-helper';
 
 const express = require('express');
 
@@ -14,6 +17,18 @@ export default class Controller {
     router.post('/generate_presigned_url', this.generatePresignedUrl);
     app.use(prefix, router);
   }
+
+  // async test(req, res) {
+  // const offer = await Offer.findByPk(59);
+  // SequelizeHelperService.syncM2M({
+  //   instance: offer,
+  //   newValues: [2, 1, 5],
+  //   targetModel: OffersOffers, // providers_datafields Path
+  //   foreignKey: 'offer_id',
+  //   otherKey: 'other_offer_id',
+  //   extra: { type: 'prerequisite' },
+  // });
+  // }
 
   generatePresignedUrl(req, res) {
     const { name } = req.body;
