e5d702cdbcc3e183b6e243c5b9fcfd6fe9ee0e57
do-kevin <do.kevin.vo@gmail.com>
2020-05-07 20:11:24 -0700

    Adds student enroll endpoint

--- server/models/student.js
+++ server/models/student.js
@@ -1,12 +1,25 @@
 export default (sequelize, DataTypes) => {
-  const Student = sequelize.define('Student', {
-    occupation: {
-      type: DataTypes.STRING,
+  const Student = sequelize.define(
+    'Student',
+    {
+      occupation: {
+        type: DataTypes.STRING,
+      },
     },
-  }, {
-    tableName: 'students',
-    underscore: true,
-  });
+    {
+      tableName: 'students',
+      underscore: true,
+    },
+  );
 
+  Student.associate = models => {
+    Student.hasMany(models.Enrollment, {
+      foreignKey: 'student_id',
+    });
+
+    Student.addScope('with_enrollments', {
+      include: [models.Enrollment],
+    });
+  };
   return Student;
 };
