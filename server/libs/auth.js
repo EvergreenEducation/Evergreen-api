de60ec557b8dd123331bc5c3624e8a295c779629
Lan Nguyen <lan-nguyen91>
2020-05-07 21:02:06 -0700

    Add Admin JSON

--- server/libs/auth.js
+++ server/libs/auth.js
@@ -7,8 +7,12 @@ import session from 'express-session';
 import util from 'util';
 import UserService from '@/services/user';
 import { sequelizeInstance } from '@/models';
+import fs from 'fs';
+import rootPath from 'app-root-path';
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const admins = JSON.parse(fs.readFileSync(`${rootPath}/admin.json`, 'utf-8'));
 
 const strategy = new Auth0Strategy(
   {
@@ -18,7 +22,7 @@ const strategy = new Auth0Strategy(
     callbackURL: env.AUTH0_CALLBACK_URL,
     state: true,
   },
-  ((accessToken, refreshToken, extraParams, profile, done) =>
+  (accessToken, refreshToken, extraParams, profile, done) =>
     /**
      * Access tokens are used to authorize users to an API
      * (resource server)
@@ -27,8 +31,7 @@ const strategy = new Auth0Strategy(
      * extraParams.id_token has the JSON Web Token
      * profile has all the information from the user
      */
-    done(null, profile)
-  ),
+    done(null, profile),
 );
 
 passport.use(strategy);
@@ -41,46 +44,55 @@ passport.deserializeUser((user, done) => {
 });
 
 export default app => {
-  app.use(session({
-    store: new SequelizeStore({
-      db: sequelizeInstance,
-      tableName: 'sessions',
+  app.use(
+    session({
+      store: new SequelizeStore({
+        db: sequelizeInstance,
+        tableName: 'sessions',
+      }),
+      secret: env.SESSION_SECRET,
+      resave: true,
+      saveUninitialized: true,
+      cookie: {
+        sameSite: false,
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      },
     }),
-    secret: env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-      sameSite: false,
-      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    },
-  }));
+  );
   app.use(passport.initialize());
   app.use(passport.session());
-  app.get(
-    '/login',
-    passport.authenticate('auth0', { scope: 'openid email' }),
-    (req, res) => {
-      res.redirect('/');
-    },
-  );
+  app.get('/login', passport.authenticate('auth0', { scope: 'openid email' }), (req, res) => {
+    res.redirect('/');
+  });
   // Perform the final stage of authentication and redirect to previously requested URL or '/user'
   app.get('/callback', (req, res, next) => {
     passport.authenticate('auth0', (err, user) => {
-      if (err) { return next(err); }
-      if (!user) { return res.redirect('/login'); }
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.redirect('/login');
+      }
 
-      return UserService.findOrCreate(user._json).then(internalUser => {
-        req.logIn(internalUser, loginErr => {
-          if (loginErr) { return next(loginErr); }
-          const { returnTo } = req.session;
-          return res.redirect(`${env.CLIENT_APP_URL}/auth/user?user_id=${internalUser.id}`);
+      const isAdmin = admins.includes(user._json.email);
+      const adminData = isAdmin ? { role: 'admin' } : {};
+
+      return UserService.findOrCreate(user._json, adminData)
+        .then(internalUser => {
+          req.logIn(internalUser, loginErr => {
+            if (loginErr) {
+              return next(loginErr);
+            }
+            const { returnTo } = req.session;
+            return res.redirect(`${env.CLIENT_APP_URL}/auth/user?user_id=${internalUser.id}`);
+          });
+        })
+        .catch(createError => {
+          if (createError.message === 'Your email has not verified.') {
+            return res.redirect(`${env.CLIENT_APP_URL}/auth/email_not_verified`);
+          }
+          return next(createError);
         });
-      }).catch(createError => {
-        if (createError.message === 'Your email has not verified.') {
-          return res.redirect(`${env.CLIENT_APP_URL}/auth/email_not_verified`);
-        }
-        return next(createError);
-      });
     })(req, res, next);
   });
 
@@ -91,9 +103,7 @@ export default app => {
     const returnTo = env.CLIENT_APP_URL;
     const port = req.connection.localPort;
 
-    const logoutURL = new url.URL(
-      util.format('https://%s/v2/logout', env.AUTH0_BASE_URL),
-    );
+    const logoutURL = new url.URL(util.format('https://%s/v2/logout', env.AUTH0_BASE_URL));
     const searchString = querystring.stringify({
       client_id: env.AUTH0_CLIENT_ID,
       returnTo,
